bb2b2632f94afdade10a482e1bc9b710efca0e0d
vinicius <viniciusbarbosa@acad.ifma.edu.br>
2022-10-27 09:05:01 -0300

    Order, Instant, ISO 8601 parte 1

--- src/main/java/com/projetosPessoais/projetoPessoal/entities/User.java
+++ src/main/java/com/projetosPessoais/projetoPessoal/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +20,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){}
 
@@ -63,6 +68,10 @@ public class User implements Serializable {
 
     public String getPassword() {
         return password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
